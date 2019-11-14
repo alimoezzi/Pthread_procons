3eb44f9fc6b608ff600a6722a2cb420db9aded89
s_arme <s.arme@ya.ru>
2019-11-14 23:43:53 +0330

    Parameterize max prodcer value

--- Pthread_procons/procon.cpp
+++ Pthread_procons/procon.cpp
@@ -3,6 +3,7 @@
 #include<cstdlib>
 
 #define BUF_SIZE 3
+#define MAX_PROD 20
 
 int buffer[BUF_SIZE]; //shared buffer
 int add = 0; //index of next element
@@ -38,7 +39,7 @@ int main() {
 
 
 void * producers(void * param) {
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i <= MAX_PROD; i++) {
 		pthread_mutex_lock(&m);
 		if (size > BUF_SIZE) exit(0); //overflow
 		while (size == BUF_SIZE) { //block if buffer is full
@@ -57,19 +58,19 @@ void * producers(void * param) {
 
 void * consumers(void * param) {
 	int i = 0;
-	do {
+	while (size > -1) {
 		pthread_mutex_lock(&m);
-		while (size == 0 and i < 19) { //block if buffer is empty
+		while (size == 0 and i < MAX_PROD) { //block if buffer is empty
 			pthread_cond_wait(&c_cons, &m);
 		}
-		if (i == 19) break;
+		if (i == MAX_PROD) break;
 		i = buffer[rem];
 		printf("consume %d @ %d\n", i, rem);
 		rem = (rem + 1) % BUF_SIZE;
 		size--;
 		pthread_mutex_unlock(&m);
 		pthread_cond_signal(&c_prod);
-	} while (size > -1);
+	}
 
 	puts("consumer ending\n");
 	return 0;
